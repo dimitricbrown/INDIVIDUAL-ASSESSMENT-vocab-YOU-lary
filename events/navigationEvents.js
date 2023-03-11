c9ee955336da656f4939d33de90c3f5ae6c4bc30
dimitricbrown <dimitricbrown@outlook.com>
2023-03-11 07:04:15 -0600

    Set Up js files and all other components

---
+++ events/navigationEvents.js
@@ -0,0 +1,29 @@
+import { signOut } from '../utils/auth';
+
+// navigation events
+const navigationEvents = () => {
+  // LOGOUT BUTTON
+  document.querySelector('#logout-button')
+    .addEventListener('click', signOut);
+
+  // TODO: CREATE VOCAB ENTRY
+  document.querySelector('#create-vocab-entry').addEventListener('click', () => {
+
+  });
+  // STRETCH: SEARCH
+  document.querySelector('#search').addEventListener('keyup', (e) => {
+    const searchValue = document.querySelector('#search').value.toLowerCase();
+    console.warn(searchValue);
+
+    // WHEN THE USER PRESSES ENTER, MAKE THE API CALL AND CLEAR THE INPUT
+    if (e.keyCode === 13) {
+    // MAKE A CALL TO THE API TO FILTER ON THE BOOKS
+    // IF THE SEARCH DOESN'T RETURN ANYTHING, SHOW THE EMPTY STORE
+    // OTHERWISE SHOW THE STORE
+
+      document.querySelector('#search').value = '';
+    }
+  });
+};
+
+export default navigationEvents;
---
+++ utils/clearDom.js
@@ -0,0 +1,7 @@
+const clearDom = () => {
+  document.querySelector('#store').innerHTML = '';
+  document.querySelector('#category-button').innerHTML = '';
+  document.querySelector('#form-container').innerHTML = '';
+};
+
+export default clearDom;
